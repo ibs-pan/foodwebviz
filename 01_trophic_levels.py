dc8aead29974199469841135b66178a7008818d5
Łukasz Pawluczuk <l.pawluczuk@adform.com>
2020-10-31 18:46:37 +0100

    visualization refactor

--- 01_trophic_levels.py
+++ 01_trophic_levels.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -14,83 +13,24 @@
 # ---
 
 # +
-import altair as alt
-import numpy as np
-import pandas as pd
-import seaborn as sns
 from foodwebs.foodweb import FoodWeb
 from foodwebs.foodweb_io import read_from_SCOR
-import networkx as nx
-from collections import defaultdict
 
-import matplotlib.pyplot as pltś
-import pylab
+
+from foodwebs.visualization import draw_trophic_flows_heatmap, show_trophic_flows_distribution
 
 from pylab import rcParams
 rcParams['figure.figsize'] = 12, 8
-
-# +
-import glob
-food_webs = [read_from_SCOR(net_path) for net_path in glob.glob('./data/*')]
-
-for web in food_webs:
-    print(f'{web.title[:30]} --> {web.n}, {web.n_living}')
-    web.node_df['trophic'] = round(web.node_df.TrophicLevel)
 # -
 
 # ## Heatmap
 
-# +
-from collections import defaultdict
-
-def get_trophic_flows(net):
-    graph = net.get_graph()
-
-    trophic_flows = defaultdict(float)
-    for n, n_trophic in set(graph.nodes(data='TrophicLevel')):
-        for m, m_trophic in set(graph.nodes(data='TrophicLevel')):
-            weight = graph.get_edge_data(n, m, default=0)
-            if weight != 0:
-                trophic_flows[(round(n_trophic), round(m_trophic))] += weight['weight']
-    return pd.DataFrame([(x, y, w) for (x, y), w in trophic_flows.items()], columns=['from', 'to', 'weights'])
-
-
-# -
-
-def draw_heatmap(net, log_scale=False):
-    '''
-    Draws heatmap of flows between trophic levels
-    '''
-    tf_pd = get_trophic_flows(net)
-    if log_scale:
-        tf_pd['weights'] = np.log(tf_pd.weights)
-    tf_pd = tf_pd.pivot('from', 'to', 'weights')
-    ax = sns.heatmap(tf_pd, annot=True, cmap="YlGnBu")
-
-
-draw_heatmap(food_webs[0])  
-
-draw_heatmap(food_webs[0], log_scale=True)
-
+draw_trophic_flows_heatmap(food_webs[0], log_scale=True)
 
 # ## Fows distribution
-
-def show_trophic_flows_distribution(net, normalize=False):
-    tf_pd = tf_pd = get_trophic_flows(net)
-    tf_pd['to'] = tf_pd['to'].astype(str)
-
-    return alt.Chart(tf_pd).mark_bar().encode(
-        x=alt.X('sum(weights):Q', stack='zero' if not normalize else 'normalize'),
-        y=alt.Y('from:N'),
-        color=alt.Color('to'),
-        order=alt.Order(
-          # Sort the segments of the bars by this field
-          'to',
-          sort='ascending'
-        )
-    ).properties(width=1320, height=400)
-
 
 show_trophic_flows_distribution(food_webs[0])
 
 show_trophic_flows_distribution(food_webs[0], normalize=True)
+
+
--- foodwebs/visualization.py
+++ foodwebs/visualization.py
@@ -1,5 +1,10 @@
 import math
+import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
+
+from collections import defaultdict
 
 
 def _get_trophic_layer(graph, from_nodes, to_nodes):
@@ -27,6 +32,19 @@ def _get_trophic_layer(graph, from_nodes, to_nodes):
     )
 
 
+def _get_trophic_flows(net):
+    ''' For each pair of trophic levels assigns sum of all weights in that pair '''
+    graph = net.get_graph()
+
+    trophic_flows = defaultdict(float)
+    for n, n_trophic in set(graph.nodes(data='TrophicLevel')):
+        for m, m_trophic in set(graph.nodes(data='TrophicLevel')):
+            weight = graph.get_edge_data(n, m, default=0)
+            if weight != 0:
+                trophic_flows[(round(n_trophic), round(m_trophic))] += weight['weight']
+    return pd.DataFrame([(x, y, w) for (x, y), w in trophic_flows.items()], columns=['from', 'to', 'weights'])
+
+
 def _get_array_order(graph, nodes, reverse=False):
     def sort_key(x): return (x[1].get('TrophicLevel', 0), x[1].get('IsAlive', 0))
     return [x[0] for x in sorted(graph.nodes(data=True), key=sort_key, reverse=reverse) if x[0] in nodes]
@@ -47,8 +65,10 @@ def show_heatmap(food_web, normalization=None, show_trophic_layer=True, add_exte
     graph = food_web.get_graph(add_external_flows, mark_alive_nodes=True, normalization=normalization)
     if switch_axes:
         to_nodes, from_nodes, z = list(zip(*graph.edges(data=True)))
+        hovertemplate = '%{x} --> %{y}: %{z:.3f}<extra></extra>'
     else:
         from_nodes, to_nodes, z = list(zip(*graph.edges(data=True)))
+        hovertemplate = '%{y} --> %{x}: %{z:.3f}<extra></extra>'
 
     z = [w['weight'] for w in z]
 
@@ -85,7 +105,11 @@ def show_heatmap(food_web, normalization=None, show_trophic_layer=True, add_exte
             ticktext=ticktext
         )
         heatmap.customdata = z_orginal
-        heatmap.hovertemplate = '%{y} --> %{x}: %{customdata:.3f}<extra></extra>'
+        if switch_axes:
+            hovertemplate = '%{x} --> %{y}: %{customdata:.3f}<extra></extra>'
+        else:
+            hovertemplate = '%{y} --> %{x}: %{customdata:.3f}<extra></extra>'
+        heatmap.hovertemplate = hovertemplate
 
     fig.add_trace(heatmap)
     fig.update_layout(title=food_web.title,
@@ -105,4 +129,75 @@ def show_heatmap(food_web, normalization=None, show_trophic_layer=True, add_exte
                       )
     fig.update_xaxes(showspikes=True, spikethickness=0.5)
     fig.update_yaxes(showspikes=True, spikesnap="cursor", spikemode="across", spikethickness=0.5)
+    fig.show()
+
+
+def draw_trophic_flows_heatmap(food_web, switch_axes=False, log_scale=False):
+    '''
+    Draws heatmap of flows between trophic levels
+    '''
+    if not switch_axes:
+        hovertemplate = '%{y} --> %{x}: %{z:.3f}<extra></extra>'
+    else:
+        hovertemplate = '%{x} --> %{y}: %{z:.3f}<extra></extra>'
+
+    tf_pd = _get_trophic_flows(food_web)
+    z = np.log10(tf_pd['weights']) if log_scale else tf_pd['weights']
+
+    heatmap = go.Heatmap(x=tf_pd['to' if not switch_axes else 'from'],
+                         y=tf_pd['from' if not switch_axes else 'to'],
+                         z=z,
+                         xgap=0.2,
+                         ygap=0.2,
+                         colorscale='Emrld',  # 'Tealgrn',
+                         hoverongaps=False,
+                         hovertemplate=hovertemplate)
+
+    if log_scale:
+        ticktext = [10**x for x in range(int(math.log10(max(tf_pd['weights']))) + 1)]
+        tickvals = range(int(math.log10(min(tf_pd['weights']))) + 1,
+                         int(math.log10(max(tf_pd['weights']))) + 1)
+
+        heatmap.colorbar = dict(
+            tick0=0,
+            tickmode='array',
+            tickvals=list(tickvals),
+            ticktext=ticktext
+        )
+        heatmap.customdata = tf_pd['weights']
+        if switch_axes:
+            hovertemplate = '%{x} --> %{y}: %{customdata:.3f}<extra></extra>'
+        else:
+            hovertemplate = '%{y} --> %{x}: %{customdata:.3f}<extra></extra>'
+        heatmap.hovertemplate = hovertemplate
+
+    fig = go.Figure(data=heatmap)
+    fig.update_layout(title=food_web.title,
+                      width=1200,
+                      height=900,
+                      autosize=True,
+                      yaxis={'title': 'From' if not switch_axes else 'To',
+                             'dtick': 1},
+                      xaxis={'title': 'To' if not switch_axes else 'From',
+                             'dtick': 1},
+                      )
+    fig.show()
+
+
+def show_trophic_flows_distribution(net, normalize=False):
+    tf_pd = _get_trophic_flows(net)
+    tf_pd['to'] = tf_pd['to'].astype(str)
+
+    if normalize:
+        tf_pd['percentage'] = tf_pd['weights'] / tf_pd.groupby('from')['weights'].transform('sum')
+
+    fig = px.bar(tf_pd,
+                 y="from",
+                 x="weights" if not normalize else "percentage",
+                 color="to",
+                 title=net.title,
+                 height=600,
+                 width=1000,
+                 template="simple_white",
+                 orientation='h')
     fig.show()
