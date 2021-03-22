fd48cf556f8d1180dc8af228c7b2e4e16823af16
Łukasz Pawluczuk <l.pawluczuk@adform.com>
2021-03-22 13:35:20 +0100

    test_io tempdir added

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,14 +1,17 @@
 import pandas as pd
+import tempfile
+import os
 
 import foodwebs as fw
 
 
 def test_write_scor(food_web):
-    scor_filename = 'test.scor'
-    fw.write_to_SCOR(food_web, scor_filename)
+    with tempfile.TemporaryDirectory() as tempdir:
+        scor_filename = os.path.join(tempdir, 'test.scor')
+        fw.write_to_SCOR(food_web, scor_filename)
 
-    web_from_file = fw.read_from_SCOR(scor_filename)
-    assert food_web.n == web_from_file.n
-    assert food_web.n_living == web_from_file.n_living
-    pd.testing.assert_frame_equal(food_web.flow_matrix, web_from_file.flow_matrix)
-    pd.testing.assert_frame_equal(food_web.node_df, web_from_file.node_df)
+        web_from_file = fw.read_from_SCOR(scor_filename)
+        assert food_web.n == web_from_file.n
+        assert food_web.n_living == web_from_file.n_living
+        pd.testing.assert_frame_equal(food_web.flow_matrix, web_from_file.flow_matrix)
+        pd.testing.assert_frame_equal(food_web.node_df, web_from_file.node_df)
