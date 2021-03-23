12159b7fe5ce0c265f857d30236e6c8f8c8807bd
Łukasz Pawluczuk <l.pawluczuk@adform.com>
2021-03-23 15:55:59 +0100

    minor fix and visual changes

--- foodwebs/utils.py
+++ foodwebs/utils.py
@@ -12,12 +12,12 @@ __all__ = [
 
 NOT_ALIVE_MARK = '\u2717'
 
+
 def calculate_trophic_levels(food_web):
-    
-    
-    '''Calculate the fractional trophic levels of nodes using their the recursive 
-    relation. This implementation uses diet matrix to improve the numerical 
-    behaviour of computation.
+    '''Calculate the fractional trophic levels of nodes using their the recursive
+    relation. This implementation uses diet matrix to improve the numerical
+    behavior of computation.
+
     In matrix form the trophic levels vector t is defined by
     t= 1 for primary producers and non-living nodes
     t= 1 + A t + b for other nodes
@@ -38,31 +38,31 @@ def calculate_trophic_levels(food_web):
 
     # the diagonal has the sum of all incoming system flows to the compartment i,
     # except flow from i to i
-    A = food_web.get_dietMatrix().transpose() 
-    
-    tl=pd.DataFrame(food_web.flowMatrix.sum(axis=0), columns=['inflow'])
-    #here we identify nodes at trophic level 1:
+    A = food_web.get_diet_matrix().transpose()
+
+    tl = pd.DataFrame(food_web.flow_matrix.sum(axis=0), columns=['inflow'])
+    # here we identify nodes at trophic level 1
     tl['is_fixed_to_one'] = (tl.inflow <= 0.0) | (np.arange(data_size) >= food_web.n_living)
     tl['data_trophic_level'] = tl.is_fixed_to_one.astype(float)
-    
-    
+
     # counting the nodes with TL fixed to 1
     if (sum(tl.is_fixed_to_one) != 0):
         # update the equation due to the prescribed trophic level 1 - reduce the dimension of the matrix
-        A_tmp = A.loc[~tl['is_fixed_to_one'],~tl['is_fixed_to_one']]
+        A_tmp = A.loc[~tl['is_fixed_to_one'], ~tl['is_fixed_to_one']]
         A_tmp = A_tmp*-1 + pd.DataFrame(np.identity(len(A_tmp)), index=A_tmp.index, columns=A_tmp.columns)
-    
+
         B = pd.DataFrame(tl[~tl.is_fixed_to_one].is_fixed_to_one.copy())
-        B['b'] = 1 #filling the constants vector with ones - the constant 1 contribution
-        B['b'] = B['b'] + A.loc[~tl['is_fixed_to_one'], tl['is_fixed_to_one']].sum(axis=1) #this is the diet fraction fron non-living denoted as b in the function description
-    
+        # filling the constants vector with ones - the constant 1 contribution
+        B['b'] = 1
+        # this is the diet fraction from non-living denoted as b in the function description
+        B['b'] = B['b'] + A.loc[~tl['is_fixed_to_one'], tl['is_fixed_to_one']].sum(axis=1)
+
         A_inverse = np.linalg.pinv(A_tmp)
         tl.loc[~tl['is_fixed_to_one'], 'data_trophic_level'] = np.dot(A_inverse, B['b'])
     else:
         # fails with negative trophic levels = some problems
         np.linalg.pinv(A)
     return tl.data_trophic_level.values
-    
 
 
 def is_alive_mapping(food_web):
