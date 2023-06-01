17b7fcec6465fff6bf2129a26cde6954eeb3f6e4
lpawluczuk <lukpawlucz@gmail.com>
2023-06-01 13:51:58 +0200

    moved form setup to pyproject

--- foodwebviz/__init__.py
+++ foodwebviz/__init__.py
@@ -4,8 +4,6 @@ foodwebviz
 foodwebviz is a Python package for the analysis and visualization of throphic networks.
 """
 
-__version__ = "0.1"
-
 from foodwebviz.io import *
 
 from foodwebviz.utils import *
